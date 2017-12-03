827cba71e3b06a26fb6293c29f6ed8f87348ae62
Harsha Vardhan Simhadri <harsha.v.simhadri@gmail.com>
2017-12-03 01:32:43 +0000

    added license headers to source code

--- include/deletedCode.h
+++ include/deletedCode.h
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
 #pragma once
 
 // Abstract base class for different Matrix representations
--- src/utils.cpp
+++ src/utils.cpp
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
 #include "utils.h"
 
 void ISLE::create_vocab_list(
