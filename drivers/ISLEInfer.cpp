5dd686b1316cfba44eb6bb67a13ece8b73ab8556
Harsha Vardhan Simhadri <harsha.v.simhadri@gmail.com>
2019-01-04 15:05:23 +0530

    infer file now takes doc begin and end ranges

--- drivers/ISLEInfer.cpp
+++ drivers/ISLEInfer.cpp
@@ -9,10 +9,10 @@ using namespace ISLE;
 // Output: list of <doc_id> <topic id> <wt>  (small wt entries will be dropped)
 int main(int argv, char**argc)
 {
-    if (argv != 11) {
+    if (argv != 12) {
         std::cout << "Incorrect usage of ISLEInfer. Use: \n"
             << "inferFromFile <sparse_model_file> <infer_file> <output_dir> "
-            << "<num_topics> <vocab_size> <num_docs_in_infer_file> "
+            << "<num_topics> <vocab_size> <min_doc_id_in_infer_file> <max_doc_id_in_infer_file>"
             << "<nnzs_in_infer_file> <nnzs_in_sparse_model_file> "
             << "<iters>[0 for default]  "
             << "Lifschitz_constant_guess>[0 for default]" << std::endl;
@@ -24,13 +24,14 @@ int main(int argv, char**argc)
 
     const doc_id_t num_topics = atol(argc[4]);
     const word_id_t vocab_size = atol(argc[5]);
-    const doc_id_t num_docs = atol(argc[6]);
-    const offset_t max_entries = atol(argc[7]);
-    const offset_t M_hat_catch_sparse_entries = atol(argc[8]);
+    const doc_id_t doc_begin = atol(argc[6]);
+    const doc_id_t doc_end = atol(argc[7]);
+    const offset_t max_entries = atol(argc[8]);
+    const offset_t M_hat_catch_sparse_entries = atol(argc[9]);
 
-    int iters = atol(argc[9]);
+    int iters = atol(argc[10]);
     if (iters == 0) iters = INFER_ITERS_DEFAULT;
-    FPTYPE Lfguess = atof(argc[10]);
+    FPTYPE Lfguess = atof(argc[12]);
     if (Lfguess == 0.0) Lfguess = INFER_LF_DEAFULT;
 
     std::cout << "Loading sparse model file: " << sparse_model_file << std::endl;
@@ -43,6 +44,7 @@ int main(int argv, char**argc)
     std::vector<DocWordEntry<count_t> > entries;
     DocWordEntriesReader reader(entries);
     reader.read_from_file(infer_file, max_entries);
+    auto num_docs = doc_end - doc_begin;
     auto infer_data = new SparseMatrix<FPTYPE>(vocab_size, num_docs);
     std::sort(			// Sort by doc first, and word second.
         entries.begin(), entries.end(),
@@ -52,13 +54,14 @@ int main(int argv, char**argc)
         std::unique(entries.begin(), entries.end(),
             [](const auto& l, const auto& r) {return l.doc == r.doc && l.word == r.word; }),
         entries.end());
+    for (auto iter : entries) iter.doc -= doc_begin;
     infer_data->populate_CSC(entries);
     infer_data->normalize_docs(true, true);
 
     auto llhs = new std::pair<FPTYPE, FPTYPE>[num_docs];
 
     // Turn the following flag on for parallel inference that works block by block
-#define PARALLEL_INFERENCE
+//#define PARALLEL_INFERENCE
 #ifdef PARALLEL_INFERENCE
     doc_id_t doc_block_size = 1000000;
     int64_t num_blocks = divide_round_up(num_docs, doc_block_size);
@@ -68,14 +71,11 @@ int main(int argv, char**argc)
         nconverged[block] = 0;
         std::cout << "Creating inference engine" << std::endl;
         ISLEInfer infer(model_by_word, infer_data, num_topics, vocab_size, num_docs);
-        /*MMappedOutput out(concat_file_path(output_dir,
-            std::string("inferred_weights_iters_") + std::to_string(iters)
-            + std::string("_Lf_") + std::to_string(Lfguess))
-            + std::string("_block_") + std::to_string(block)); */
         MMappedOutput top_out(concat_file_path(output_dir,
             std::string("top_topics_iters_") + std::to_string(iters)
-            + std::string("_Lf_") + std::to_string(Lfguess)) 
-            + std::string("_block_") + std::to_string(block));
+            + std::string("_Lf_") + std::to_string(Lfguess))
+            + std::string("_doc_") + std::to_string(doc_begin)
+            + std::string("_to_") + std::to_string(doc_end));
 
         FPTYPE* wts = new FPTYPE[num_topics];
         std::vector<std::pair<doc_id_t, FPTYPE> > top_topics;
@@ -83,7 +83,8 @@ int main(int argv, char**argc)
         for (doc_id_t doc = block*doc_block_size; doc < (block + 1)*doc_block_size && doc < num_docs; ++doc) {
             if (doc % 10000 == 9999)
                 std::cout << "docs inferred: ["
-                << (((int64_t)doc - (int64_t)10000) > (int64_t)(block*doc_block_size) ? ((int64_t)doc - (int64_t)10000) : block*doc_block_size)
+                << doc_begin + (((int64_t)doc - (int64_t)10000) > (int64_t)(block*doc_block_size) 
+                    ? ((int64_t)doc - (int64_t)10000) : block*doc_block_size)
                 << ", " << doc << "]" << std::endl;
 
             llhs[doc] = infer.infer_doc_in_file(doc, wts, iters, Lfguess);
@@ -102,7 +103,7 @@ int main(int argv, char**argc)
             std::sort(top_topics.begin(), top_topics.end(),
                 [](const auto& l, const auto& r) {return l.second > r.second; });
             for (int i = 0; i < 5 && i < top_topics.size(); ++i) {
-                top_out.concat_int(doc + 1, '\t');
+                top_out.concat_int(doc + 1 + doc_begin, '\t');
                 top_out.concat_int(1 + top_topics[i].first, '\t');
                 top_out.concat_float(top_topics[i].second, '\n');
             }
@@ -148,7 +149,7 @@ int main(int argv, char**argc)
         std::sort(top_topics.begin(), top_topics.end(),
             [](const auto& l, const auto& r) {return l.second > r.second; });
         for (int i = 0; i < 5 && i < top_topics.size(); ++i) {
-            top_out.concat_int(doc+1, '\t');
+            top_out.concat_int(doc+1+doc_begin, '\t');
             top_out.concat_int(top_topics[i].first + 1, '\t');
             top_out.concat_float(top_topics[i].second, '\n');
         }
